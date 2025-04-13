4db388056451e16d67cae6bbc00cda3731784ca8
Muhammad Arsalan Siddiqui <marsalans@outlook.com>
2025-04-14 01:24:56 +0500

    Update 03.py

--- python/Home Work Project/01/03.py
+++ python/Home Work Project/01/03.py
@@ -3,11 +3,6 @@ Converts feet to inches. Feet is an American unit of measurement.
 There are 12 inches per foot. Foot is the singular, and feet is the plural.
 """
 
-
-"""
-An example program with constants
-"""
-
 INCHES_IN_FOOT: int = 12  # Conversion factor. There are 12 inches for 1 foot.
 
 def main():
